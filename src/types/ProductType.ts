24663bcc57b7d1fa337c5f23d499f8b1acd021b3
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2024-03-05 22:20:51 +0500

    Fix some issues 5

--- src/types/ProductType.ts
+++ src/types/ProductType.ts
@@ -1,10 +1,10 @@
 export default interface ProductType {
-    model_title: string,
-    release_year: string,
-    retail_price: string,
-    product_desc: string,
-    category_title: string,
-    product_image: string | File | Blob,
+    model_title: string | boolean,
+    release_year: string | boolean,
+    retail_price: string | boolean,
+    product_desc: string | boolean,
+    category_title: string | boolean,
+    product_image: string | File | Blob | boolean,
     product_visibility: boolean | string | undefined,
-    created_at: string
+    created_at: string | boolean
 }
