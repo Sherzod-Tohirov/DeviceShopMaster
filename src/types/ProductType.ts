3c52a25a8eeff58fcbca795bc09c28081e00875e
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2024-03-05 22:12:36 +0500

    Fix some issues 3

--- src/types/ProductType.ts
+++ src/types/ProductType.ts
@@ -1,10 +1,10 @@
 export default interface ProductType {
     model_title: string,
-    release_year: string | Blob | unknown,
-    retail_price: string | Blob | unknown,
+    release_year: unknown,
+    retail_price: unknown,
     product_desc: string,
     category_title: string,
-    product_image: string | File | Blob | unknown,
+    product_image: unknown,
     product_visibility: boolean | string | undefined,
     created_at: string
 }
